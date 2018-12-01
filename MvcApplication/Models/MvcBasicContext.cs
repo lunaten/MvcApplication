e3b2c47d0f98fdf9b2eaec979ee91bf7ed61b213
lunaten <lunaten2@gmail.com>
2018-12-01 21:49:27 +0900

    Scaffoldingエラー回避のためcodeConfigurationTypeの設定をGlobal.asxs移動

--- MvcApplication/Models/MvcBasicContext.cs
+++ MvcApplication/Models/MvcBasicContext.cs
@@ -8,12 +8,18 @@ namespace MvcApplication.Models
 {
     /*
      * DbContextを継承する（要System.Data.SqlClientの参照追加）
-     * Web.Configに設定したので、今はコメント中
+     * 
+     * DbConfigurationTypeについて：
+     * Web.Configに設定してたが、Scaffoldingでエラーが発生するため、
+     * MvcBasicContextへ設定中。Global.asaxの設定でも問題ない。
      */
-    //[DbConfigurationType(typeof(MySqlEFConfiguration))]    
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]    
 
     public class MvcBasicContext : DbContext
     {
+        public MvcBasicContext() : base("DefaultConnection")
+        {
+        }
         //DbSet<データモデル>型のプロパティを定義する（名前は複数形）
         public DbSet<Member> Members { get; set; }
     }
