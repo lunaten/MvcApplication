acb07a31ca20ae6cb3bcb8583e0e87d8e845a125
lunaten <lunaten2@gmail.com>
2018-12-02 16:56:18 +0900

    Memberプロパティの表示名を設定

--- MvcApplication/Models/Member.cs
+++ MvcApplication/Models/Member.cs
@@ -1,17 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel;
 
 namespace MvcApplication.Models
 {
     public class Member
     {
         public int Id { get; set; }
+
+        [DisplayName("氏名")]
         public string Name { get; set; }
+
+        [DisplayName("メールアドレス")]
         public string Email { get; set; }
+
+        [DisplayName("誕生日")]
         public DateTime Birth { get; set; }
+
+        [DisplayName("既婚")]
         public bool Married { get; set; }
+
+        [DisplayName("自己紹介")]
         public string Memo { get; set; }
     }
 }
